74f780c340fcfec4c0f41416b96f19a5a7a74c3b
Nate Clark <natec425@gmail.com>
2018-02-07 15:22:06 -0600

    move postgres and redis setup to shell script

---
+++ setup_postgres_and_redis.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install postgresql
+sudo -u postgres creatuser -dRS basecamp
+createdb
+createdb news
+createdb forums -f forum/forum.sql
+
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+make install
